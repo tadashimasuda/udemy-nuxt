683d2bb690a47e3b88b236b346aa08fb6c9929fe
tadashimasuda <tmasu4260@gmail.com>
2020-10-17 14:21:17 +0900

    create _id page

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1>{{post.title}}</h1>
+        <pre>{{post.body}}</pre>
+        <nuxt-link to="/posts">Back to posts</nuxt-link>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return{
+            post:''
+        }
+    },
+    async asyncData({params}){
+        let {data} = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+        return {post:data}
+    },
+    head(){
+        return {
+            title:this.post.title
+        }
+    }
+}
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -4,44 +4,28 @@
             <h2>Making API request - the Vue way</h2>
             <hr>
         </div>
-        <div class="container">
-            <h4 v-for="post in posts" :key="post.id">{{post.title}}</h4>
+        <div class="container row">
+            <Card v-for="post in posts" :key="post.id" :post ="post" class="ml-auto mr-auto"/>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Card from '@/components/Card'
 
 export default {
+     components :{
+        Card
+    },
     data(){
         return{
             posts:''
         }
     },
     async asyncData(){
-        let {data} = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
         return {posts:data}
     },
 }
-// import axios from 'axios'
-
-// export default {
-//     data(){
-//         return{
-//             posts:[]
-//         }
-//     },
-//     mounted(){
-//         axios.get('https://jsonplaceholder.typicode.com/todos')
-//         .then(response => {
-//             console.log(response.data)
-//             this.posts = response.data
-//         })
-//         .catch(error => {
-//             console.log(error)
-//         }) 
-
-//     }
-// }
 </script>
